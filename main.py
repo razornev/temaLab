7dadfc227b348e04f6c7bcfd8fe426a0353d2b07
razornev <34588296+razornev@users.noreply.github.com>
2019-11-25 15:41:05 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,68 +2,42 @@ from keras.layers import Conv2D, Dense, Flatten, MaxPool2D, LeakyReLU
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.losses import categorical_crossentropy
+from DataGenerator import DataGenerator
 import cv2
 import numpy as np
 
 
-# 261 mercedes 0-260 idx
-# 168 toyota 0-169 idx
-# 131 volvo 0-130 idx
-
-mercNum = 10
-toyNum = 10
-volvNum = 10
-
-fileNames = []
-
-brandLengths = {"mercedes":mercNum, "toyota":toyNum, "volvo":volvNum}
-
-fileNamesIndex = 0
-for brand in brandLengths:
-    for i in range(0, brandLengths[brand]):
-        fileNames.insert(fileNamesIndex, "padded" + brand + "-" + str(i) + ".jpg")
-        fileNamesIndex += 1
-
-np.random.shuffle(fileNames)
-
-x = []
-y = fileNames
-
-maxHeight = 0
-maxWidth = 0
-
-
-for fileName in y:
-    if("volvo" in fileName): brand = "volvo"
-    elif("mercedes" in fileName): brand = "mercedes"
-    else: brand = "toyota"
-    index = y.index(fileName)
-    origFileName = fileName
-    if brand is 'volvo':
-        y[index] = (1, 0, 0)
-    elif brand is 'mercedes':
-        y[index] = (0, 1, 0)
-    else:
-        y[index] = (0, 0, 1)
-    #x.append(cv2.cvtColor(cv2.imread("./reducedTrainSet/"+brand+"/"+origFileName), cv2.COLOR_BGR2GRAY))
-    x.append(cv2.imread("./reducedTrainSet/" + brand + "/" + origFileName))
-
-x = np.array(x)
-y = np.array(y)
-
+# 261 mercedes 0-260 idx    train:240   test:21
+# 168 toyota 0-169 idx      train:150   test:18
+# 131 volvo 0-130 idx       train:120   test:11
+# 560 - 50  = 510
 
 model = Sequential([
     Conv2D(8, (100, 100), strides=8, padding="valid", input_shape=(2122, 4209, 3), activation='relu'),
+    Conv2D(8, (30, 30), strides=8, padding="valid", activation='relu'),
     MaxPool2D(pool_size=(3, 3)),
+    MaxPool2D(pool_size=(2, 2)),
     Flatten(),
-    Dense(8, activation='relu'),
+    Dense(30, activation='relu'),
     Dense(3, activation='softmax')
 ])
 
 model.compile("adam", categorical_crossentropy, [categorical_crossentropy])
 
-for i in range(0, 10):
-    model.fit(x, y, batch_size=1, epochs=10, verbose=1)
+labels = np.load("./data/validationDictionary.npy", allow_pickle=True).item()
 
-print(y)
-print(model.predict(x))
+print(labels)
+
+list_IDs = np.arange(0, labels.__len__(), 1)
+
+'''for i in range(0, len(list_IDs)) :
+    list_IDs[i] = str(list_IDs[i])
+'''
+
+model.fit_generator(DataGenerator(list_IDs, labels, n_classes=3), epochs=10)
+
+
+'''
+sources:
+https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly#data-generator
+'''
